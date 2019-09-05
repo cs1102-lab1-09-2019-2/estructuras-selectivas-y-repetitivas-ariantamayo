7f1b6a82fdebeac47c569e72caba2f8468c7ca55
Arián Tamayo <54276418+ariantamayo@users.noreply.github.com>
2019-09-05 12:34:53 -0500

    exercise 3 solved

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,6 +1,23 @@
-
-
+#include <iostream>
+#include <vector>
+using namespace std;
 int main() {
-
-    return 0;
+ int num;
+ int mayor;
+ cout << "n: ";
+ cin >> num;
+vector<int> asd(num,0);
+for (int i=0; i<num;i++){
+  cin >> asd[i];
+}
+mayor = asd[0];
+for (int i=0; i<num;i++){
+  if (mayor<asd[i]){
+    mayor = asd [i];
+  }else{
+    mayor = mayor;
+  }
+}
+cout << "El mayor es: " << mayor;
+return 0;
 }
