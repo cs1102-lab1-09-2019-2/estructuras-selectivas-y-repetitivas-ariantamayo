916f0117a70c556b77fdc89a5e81a84fe951b3d8
Arián Tamayo <54276418+ariantamayo@users.noreply.github.com>
2019-09-05 12:33:53 -0500

    exercise 2 solved

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,6 +1,27 @@
-
-
+#include <iostream>
+using namespace std;
 int main() {
+  int num;
+  cout<<"Ingrese un número: ";
+  cin >> num;
+  for (int i=0; i<num;i++){
+    cout << "*";
+  }
+  cout << "\n";
+  for (int i=0; i<num-2;i++){
+    cout << "*";
+    for (int j=0; j<i;j++){
+    cout << " "; 
+  }
+  cout << "*";
+  for (int j=0; j<num-3-i;j++){
+    cout << " "; 
+  }
 
-    return 0;
+  cout << "*\n";
+  }
+  for (int i=0; i<num;i++){
+    cout << "*";
+  }
+return 0;    
 }
