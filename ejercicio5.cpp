854122c848146458f1a3b3d694474613fa3f366d
Arián Tamayo <54276418+ariantamayo@users.noreply.github.com>
2019-09-05 12:44:05 -0500

    exercise 5 solved

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,6 +1,17 @@
-
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
 
 int main() {
-
-    return 0;
+    float cant, temp, media, suma = 0, sumax = 0;
+    cout<<"Ingrese el número a manipular: ";
+    cin>>cant;
+    for(int i = 0; i < cant; i++){
+        cout<<'x'<<i+1<<": ";
+        cin>>temp;
+        suma += temp;
+        sumax += pow(temp, 2);
+    }
+    cout<<"la desviación estándar es: "<<fixed<<setprecision(2)<<sqrt(sumax/cant - pow(suma/cant,2));
 }
